16e32c3b49dd97e5f204484e591b7a7994126eeb
Developer-JoaoVictor <joaoliveira.contato@hotmail.com>
2025-07-20 13:33:51 -0300

    fix: Corrigi a porta para o deploy do render

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app'
 import { env } from './env'
+
 app
   .listen({
     port: env.PORT,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP Server Running')
